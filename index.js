0ca3349a3524a3f6ac57253aaf78f306ddca610c
Luke <luke@lukeraybould.dev>
2026-05-27 23:08:33 +0100

    Add shape drop logic. Adds enums for the type of shape and quadrant, maps between them, and uses this to set the correct shape for the quadrant

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
 "use strict";
 (function () {
+    const SHAPES = {
+        SQUARE: "square",
+        TRIANGLE: "triangle",
+        HEXAGON: "hexagon",
+        CIRCLE: "circle" 
+    };
+
+    const QUADRANT = {
+        TOP_LEFT: "top-left",
+        TOP_RIGHT: "top-right",
+        BOTTOM_LEFT: "bottom-left",
+        BOTTOM_RIGHT: "bottom-right" 
+    };
+    
     (function _init() {
         _bindEvents();
     })();
@@ -21,7 +35,28 @@
     }
 
     function _onDragStop(event, ui) {
-        alert("AAA");
+        const shape = ui.draggable;
+        const quadrant = jQuery(this);
+
+        const newShapeClass = _getShapeClassByQuadrant(quadrant);
+
+        shape.removeClass("circle square hexagon triangle");
+        shape.addClass(newShapeClass);
     }
-    
+
+    function _getShapeClassByQuadrant(quadrant) {
+        if (quadrant.hasClass(QUADRANT.TOP_LEFT)) {
+            return SHAPES.CIRCLE;
+        }
+        else if (quadrant.hasClass(QUADRANT.TOP_RIGHT)) {
+            return SHAPES.HEXAGON;
+        }
+        else if (quadrant.hasClass(QUADRANT.BOTTOM_LEFT)) {
+            return SHAPES.SQUARE;
+        }
+        else if (quadrant.hasClass(QUADRANT.BOTTOM_RIGHT)) {
+            return SHAPES.TRIANGLE;
+        }
+    }
+
 })();
